05a227ee9236ea5261b87a3c8994fa2be34cd4ac
rohitajay <rohit.gupta@cognologix.com>
2023-04-22 17:48:59 +0530

    updated database settings for postgres connect

--- core/settings.py
+++ core/settings.py
@@ -78,8 +78,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_database',
+        'USER': 'postgres',
+        'PASSWORD': '9419131674',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
